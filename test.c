91712c9c202b0f53c8be60f031c19e61e8c8c3be
Javier Lazo-Arevalo <javieralejandrolazoarevalo@gmail.com>
2022-11-21 10:57:03 -0500

    worked out how to find a file using relative path finding

--- test.c
+++ test.c
@@ -1,7 +1,17 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 int main()
 {
+    FILE* textfile;  //creates a variable to call and manipulate the file
+    char character;
+    textfile=fopen("../../../Downloads/path_tester.txt", "r");    //opens the file to read when given the relative path
+    do
+    {
+        character = fgetc(textfile); //gets a single character
+        printf("%c", character);
+    }
+    while (character != EOF);
     return 0;
 }
