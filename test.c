8d24c7353ffbbbbd3d4c25a4b2550c177a936590
Javier Lazo-Arevalo <javieralejandrolazoarevalo@gmail.com>
2022-11-27 22:31:45 -0500

    completed the gps entry more manual mode and it works

--- test.c
+++ test.c
@@ -10,13 +10,16 @@ struct position
     double longitude;
     double altitude;
 };
-void addposition(struct position *source, int *n)
+void addposition(struct position *source, int *n, double *dist)
 {
     char name [25];
     double time;
     double latitude;
     double longitude;
     double altitude;
+    double distance;
+    struct position thisdistance;
+    struct position original;
     printf("enter person name:\n");
     scanf("%s",&name);
     printf("enter time:\n");
@@ -34,12 +37,31 @@ void addposition(struct position *source, int *n)
     thisposition.longitude = longitude;
     thisposition.altitude = altitude;
     source[*n]=thisposition;
+    thisdistance=source[*n];
+    original=source[0];
+    double dislat=thisdistance.latitude;
+    double origlat=original.latitude;
+    dislat=(dislat-origlat);
+    dislat=pow((dislat),2);
+    double dislon=thisdistance.longitude;
+    double origlon=original.longitude;
+    dislon=(dislon-origlon);
+    dislon=pow((dislon),2);
+    double disalt=thisdistance.altitude;
+    double origalt=original.altitude;
+    disalt=(disalt-origalt);
+    disalt=pow((disalt),2);
+    distance=sqrt(dislat+dislon+disalt);
+    dist[*n]=distance;
     *n=*n+1;
 }
 struct position people[20];
 int main()
 {
+    double dist[20];
+    dist[0]=0;
     int check;
+    int spot;
     printf("Would you like to enter the data manually or from a file?\nEnter '1' for manual and '2' for from a file\n");
     scanf("%d",&check);
     if (check==1)
@@ -49,24 +71,31 @@ int main()
         scanf("%d",&others);
         others+=1;
         printf("please enter your position\n");
-        addposition(people,&num);
+        addposition(people,&num,dist);
         printf("currently %d people added\n",num);
         for (int i = 1; i < others; i++)
         {
             printf("enter other persons position please\n");
-            addposition(people,&num);
+            addposition(people,&num,dist);
             printf("now %d entry\n", num);
         }
-        for (int i = 0; i < num; i++)
+        double temp=dist[1];
+        for (int i = 1; i < num; i++)
         {
-            struct position thisguy = people[i];
-            printf("person name; %s\n", thisguy.name);
-            printf("person time; %.2lf\n", thisguy.time);
-            printf("person latitude; %.2lf\n", thisguy.latitude);
-            printf("person longitude; %.2lf\n", thisguy.longitude);
-            printf("person altitude; %.2lf\n", thisguy.altitude);
+            if (temp>dist[i])
+            {
+                temp=dist[i];
+            }
         }
-        
+        for (int i = 1; i < num; i++)
+        {
+            if (temp==dist[i])
+            {
+                spot=i;
+            }
+        }
+        struct position closest = people[spot];
+        printf("The closes person to the first entry is at latitude %.2lf, longitude %.2lf, altitude %.2lf and their name is %s.", closest.latitude, closest.longitude, closest.altitude, closest.name);
     }
     else if (check==2)
     {
